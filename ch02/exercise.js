5f08e7838bf81d2cc12ab0042c0d66b4d22705fe
Abbes Boulebtina <abbes.boulebtina@gmail.com>
2025-02-22 11:07:15 -0500

    Exercise

--- ch02/exercise.js
+++ ch02/exercise.js
@@ -17,13 +17,21 @@ async function additionner(a, b) {
   return a + b;
 }
 
+async function additionner1(a, b) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(a + b); // Résoudre la Promise après 10 secondes avec le résultat
+    }, 10000);
+  });
+}
+
 // Exemple d'utilisation avec then/catch
-additionner(5, 10)
+additionner(5, 15)
   .then((resultat) => {
     console.log("Résultat avec then/catch:", resultat);
   })
   .catch((erreur) => {
-    console.error("Erreur:", erreur);
+    console.error("Erreur:", erreur.message);
   });
 
 // Exemple d'utilisation avec async/await
@@ -38,3 +46,22 @@ async function utiliserAdditionner() {
 
 utiliserAdditionner();
 
+console.log("Fin du programme");
+
+// Exemple fetch asynchrone
+
+async function fetchData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
+
+fetchData();
+console.log("Find du programme");
